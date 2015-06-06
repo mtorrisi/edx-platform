8e250a651c74d0b76de812657e1899bbeda95477
Sarina Canelake <sarina@edx.org>
2015-06-05 23:46:21 -0400

    dark_lang: only allow released langs in accept header LOC-72, LOC-85
    Only return languages we've actually released LOC-85
    Perform fuzzy matching to greedily serve the best released language LOC-72

--- common/djangoapps/dark_lang/middleware.py
+++ common/djangoapps/dark_lang/middleware.py
@@ -120,11 +120,20 @@ class DarkLangMiddleware(object):
         self._clean_accept_headers(request)
         self._activate_preview_language(request)
 
-    def _is_released(self, lang_code):
-        """
-        ``True`` iff one of the values in ``self.released_langs`` is a prefix of ``lang_code``.
-        """
-        return any(lang_code.lower().startswith(released_lang.lower()) for released_lang in self.released_langs)
+    def _fuzzy_match(self, lang_code):
+        """Returns a fuzzy match for lang_code"""
+        print "_fuzzy_match: Checking if {} == any of {}".format(lang_code.lower(), self.released_langs)
+        if lang_code in self.released_langs:
+            print 'Exact match; found {} in released langs'.format(lang_code)
+            return lang_code
+
+        lang_prefix = lang_code.split('-')[0]
+        for released_lang in self.released_langs:
+            released_prefix = released_lang.split('-')[0]
+            if lang_prefix == released_prefix:
+                print "Fuzzy match: Asked for {}, returning {}".format(lang_code, released_lang)
+                return released_lang
+        return False
 
     def _format_accept_value(self, lang, priority=1.0):
         """
@@ -141,13 +150,17 @@ class DarkLangMiddleware(object):
         if accept is None or accept == '*':
             return
 
-        new_accept = ", ".join(
-            self._format_accept_value(lang, priority)
-            for lang, priority
-            in dark_parse_accept_lang_header(accept)
-            if self._is_released(lang)
-        )
+        print "Getting new acceptance headers"
 
+        new_accept = []
+        for lang, priority in dark_parse_accept_lang_header(accept):
+            fuzzy_code = self._fuzzy_match(lang.lower())
+            if fuzzy_code:
+                new_accept.append(self._format_accept_value(fuzzy_code, priority))
+
+        new_accept = ", ".join(new_accept)
+
+        print 'setting meta accept to: "{}"'.format(new_accept), type(new_accept)
         request.META['HTTP_ACCEPT_LANGUAGE'] = new_accept
 
     def _activate_preview_language(self, request):

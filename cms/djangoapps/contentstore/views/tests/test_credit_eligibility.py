280d15927426c5ea4bce7a62cfb0ea2f9449cd7e
zubair-arbi <zubair.afzal@arbisoft.com>
2015-06-08 17:23:38 +0500

    fix failing test for credit eligibilty requirements

--- cms/djangoapps/contentstore/views/tests/test_credit_eligibility.py
+++ cms/djangoapps/contentstore/views/tests/test_credit_eligibility.py
@@ -2,36 +2,28 @@
 Unit tests for credit eligibility UI in Studio.
 """
 
-from mock import patch
+import mock
 
 from contentstore.tests.utils import CourseTestCase
 from contentstore.utils import reverse_course_url
 from xmodule.modulestore.tests.factories import CourseFactory
 
+from openedx.core.djangoapps.credit.api import get_credit_requirements
 from openedx.core.djangoapps.credit.models import CreditCourse
+from openedx.core.djangoapps.credit.signals import listen_for_course_publish
 
 
 class CreditEligibilityTest(CourseTestCase):
     """Base class to test the course settings details view in Studio for credit
-    eligibility.
+    eligibility requirements.
     """
-    def setUp(self, **kwargs):
-        enable_credit_eligibility = kwargs.get('enable_credit_eligibility', False)
-        with patch.dict('django.conf.settings.FEATURES', {'ENABLE_CREDIT_ELIGIBILITY': enable_credit_eligibility}):
-            super(CreditEligibilityTest, self).setUp()
-
+    def setUp(self):
+        super(CreditEligibilityTest, self).setUp()
         self.course = CourseFactory.create(org='edX', number='dummy', display_name='Credit Course')
         self.course_details_url = reverse_course_url('settings_handler', unicode(self.course.id))
 
-
-class CreditEligibilityDisabledTest(CreditEligibilityTest):
-    """Test the course settings details view response when feature flag
-    'ENABLE_CREDIT_ELIGIBILITY' is not enabled.
-    """
-    def setUp(self):
-        super(CreditEligibilityDisabledTest, self).setUp(enable_credit_eligibility=False)
-
-    def test_get_method_without_enable_feature_flag(self):
+    @mock.patch.dict("django.conf.settings.FEATURES", {'ENABLE_CREDIT_ELIGIBILITY': False})
+    def test_course_details_with_disabled_setting(self):
         """Test that user don't see credit eligibility requirements in response
         if the feature flag 'ENABLE_CREDIT_ELIGIBILITY' is not enabled.
         """
@@ -40,15 +32,8 @@ class CreditEligibilityDisabledTest(CreditEligibilityTest):
         self.assertNotContains(response, "Credit Eligibility Requirements")
         self.assertNotContains(response, "Steps needed for credit eligibility")
 
-
-class CreditEligibilityEnabledTest(CreditEligibilityTest):
-    """Test the course settings details view response when feature flag
-    'ENABLE_CREDIT_ELIGIBILITY' is enabled.
-    """
-    def setUp(self):
-        super(CreditEligibilityEnabledTest, self).setUp(enable_credit_eligibility=True)
-
-    def test_get_method(self):
+    @mock.patch.dict("django.conf.settings.FEATURES", {'ENABLE_CREDIT_ELIGIBILITY': True})
+    def test_course_details_with_enabled_setting(self):
         """Test that credit eligibility requirements are present in
         response if the feature flag 'ENABLE_CREDIT_ELIGIBILITY' is enabled.
         """
@@ -60,9 +45,14 @@ class CreditEligibilityEnabledTest(CreditEligibilityTest):
         self.assertNotContains(response, "Steps needed for credit eligibility")
 
         # verify that credit eligibility requirements block shows if the
-        # course is set as credit course
+        # course is set as credit course and it has eligibility requirements
         credit_course = CreditCourse(course_key=unicode(self.course.id), enabled=True)
         credit_course.save()
+        self.assertEqual(len(get_credit_requirements(self.course.id)), 0)
+        # test that after publishing course, minimum grade requirement is added
+        listen_for_course_publish(self, self.course.id)
+        self.assertEqual(len(get_credit_requirements(self.course.id)), 1)
+
         response = self.client.get_html(self.course_details_url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Credit Eligibility Requirements")
